91a8942815d13d8de8f2092903542c56f90dd016
Renan Nunes <142848799+RenanNunesz10@users.noreply.github.com>
2026-09-08 20:28:31 -0300

    refactor: move tab content to data array

--- FirstAppReact/src/App.jsx
+++ FirstAppReact/src/App.jsx
@@ -1,8 +1,36 @@
+import Logo from "./assets/React-logo.png";
+
+const content = [
+  [
+    "React is extremely popular",
+    "It makes building complex, interactive UIs a breeze",
+    "It's powerful & flexible",
+    "It has a very active and versatile ecosystem"
+  ],
+  [
+    "Components, JSX & Props",
+    "State",
+    "Hooks (e.g., useEffect())",
+    "Dynamic rendering"
+  ],
+  [
+    "Official web page (react.dev)",
+    "Next.js (Fullstack framework)",
+    "React Native (build native mobile apps with React)"
+  ],
+  [
+    "Vanilla JavaScript requires imperative programming",
+    "Imperative Programming: You define all the steps needed to achieve a result",
+    "React on the other hand embraces declarative programming",
+    "With React, you define the goal and React figures out how to get there"
+  ]
+];
+
 function App() {
   return (
     <div>
       <header>
-        <img src="react-logo-xs.png" alt="React logo" />
+        <img src={Logo} alt="React logo" />
         <div>
           <h1>React.js</h1>
           <p>i.e., using the React library for rendering the UI</p>
